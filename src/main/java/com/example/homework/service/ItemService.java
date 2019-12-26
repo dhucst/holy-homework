4d28243dcb6c405cb550bae5b9c0a0e0c6f03994
Cyprinus <Cyprinus12138@gmail.com>
2019-12-26 11:33:54 +0800

    Add
    Add item ***.

--- src/main/java/com/example/homework/service/ItemService.java
+++ src/main/java/com/example/homework/service/ItemService.java
@@ -3,6 +3,7 @@ package com.example.homework.service;
 import com.example.homework.entity.Item;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 import javax.annotation.Resource;
 
@@ -17,21 +18,53 @@ public class ItemService {
         try {
             jdbcTemplate.update(sql, item.getItem_name(), item.getUnit(), item.getPrice(), item.getPic_url());
         } catch (Exception e) {
-            System.out.println(e);
             return false;
         }
         return true;
     }
 
     public boolean update(Item item, int id) {
-        String sql = "UPDATE  item SET item_name=? WHERE item_id=?";
+        String sql = "UPDATE  item SET item_name=?,price=?,pic_url=? WHERE item_id=?";
         try {
-            jdbcTemplate.update(sql, item.getItem_name(), id);
+            jdbcTemplate.update(sql, item.getItem_name(), item.getPrice(), item.getPic_url(), id);
         } catch (Exception e) {
             return false;
         }
         return true;
     }
 
+    public List<Item> findItemById(int id){
+        String sql = "SELECT * FROM item WHERE item_id=" + id;
+        try {
+            return jdbcTemplate.query(sql, (resultSet, i) -> {
+                Item item = new Item();
+                item.setItem_name(resultSet.getString("item_name"));
+                item.setUnit(resultSet.getString("unit"));
+                item.setPic_url(resultSet.getString("pic_url"));
+                item.setItem_id(resultSet.getInt("item_id"));
+                item.setPrice(resultSet.getInt("price"));
+                return item;
+            });
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    public List<Item> getAllItems(){
+        String sql = "SELECT * FROM item";
+        try {
+            return jdbcTemplate.query(sql, (resultSet, i) -> {
+                Item item = new Item();
+                item.setItem_name(resultSet.getString("item_name"));
+                item.setUnit(resultSet.getString("unit"));
+                item.setPic_url(resultSet.getString("pic_url"));
+                item.setItem_id(resultSet.getInt("item_id"));
+                item.setPrice(resultSet.getInt("price"));
+                return item;
+            });
+        }catch (Exception e){
+            return null;
+        }
+    }
 
 }
